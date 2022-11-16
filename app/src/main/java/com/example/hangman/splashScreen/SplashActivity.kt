e43e4e5ce6b388ffc1f3d359e4ebb2b01274e49f
Ivan Sales <ivan.sales@enti.cat>
2022-11-16 22:36:30 +0100

    Splash ya funcionando

--- app/src/main/java/com/example/hangman/splashScreen/SplashActivity.kt
+++ app/src/main/java/com/example/hangman/splashScreen/SplashActivity.kt
@@ -1,8 +1,12 @@
 package com.example.hangman.splashScreen
 
 import android.animation.ObjectAnimator
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.hangman.LoginActivity
+import com.example.hangman.MainActivity
 import com.example.hangman.databinding.ActivityLoginBinding
 import com.example.hangman.databinding.ActivitySplashBinding
 
@@ -16,6 +20,17 @@ class SplashActivity : AppCompatActivity() {
         binding.splashProgressBar.max=100
         val currentValue=100
 
-        ObjectAnimator.ofInt(binding.splashProgressBar, "progress", currentValue).setDuration(2000).start()
+        //ObjectAnimator.ofInt(binding.splashProgressBar, "progress", currentValue).setDuration(2000).start()
+        var value=0
+        Thread(Runnable {
+            while (value<=100) {
+                value+=5
+                Thread.sleep(100)
+                binding.splashProgressBar.progress = value
+            }
+            val intent = Intent(this@SplashActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }).start()
     }
 }
