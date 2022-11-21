553a0b4625b4f8b8ccf9aeb74eeec8e660e8df92
Josep <josep.romera@enti.cat>
2022-11-21 23:08:26 +0100

    Iniciado juego en kotlin y puesto contraints a todo

--- app/src/main/java/com/example/hangman/GameActivity/GameActivity.kt
+++ app/src/main/java/com/example/hangman/GameActivity/GameActivity.kt
@@ -1,4 +1,18 @@
 package com.example.hangman.GameActivity
 
-class GameActivity {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.hangman.databinding.ActivityGameBinding
+import com.example.hangman.databinding.ActivitySettingsBinding
+
+
+class GameActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityGameBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityGameBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
 }
