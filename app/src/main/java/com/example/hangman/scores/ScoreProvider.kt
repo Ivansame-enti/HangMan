58a8b57782b3bd64de715393dd310d5466fb27a8
Pablo <pperpinyanc@gmail.com>
2023-01-10 19:42:47 +0100

    gitflow-feature-stash: PrintearValoresRamaDefONo

--- app/src/main/java/com/example/hangman/scores/ScoreProvider.kt
+++ app/src/main/java/com/example/hangman/scores/ScoreProvider.kt
@@ -1,8 +1,38 @@
 package com.example.hangman.scores
 
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
 class ScoreProvider {
 
+
     companion object{
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("Players")
+        var value: Map<*, *>? = null
+
+        public fun GetData(){
+
+            myRef.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                     value = dataSnapshot.value as Map<*, *>? //Recoge datos
+                    Log.d("DATA", "Value is: " + value)
+
+                    for(key in value?.keys!!) {
+                        Log.d("DATA", "Recorre Hashmap")
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+
+                    Log.w("DATA", "Failed to read value.", error.toException())
+                }
+            })
+
+        }
 
         var scoreListDef = listOf(
             ScoreList(
@@ -19,4 +49,11 @@ class ScoreProvider {
             )
         )
     }
+
+
+
+
+
+
+
 }
